ac7a9db0e8332843287f12535d04ebc154bb4793
fckolob <131497920+fckolob@users.noreply.github.com>
2023-10-16 23:08:50 -0300

    w5 programming tasks

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,48 +1,34 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-const templesElement = document.getElementById("temples");
+
 let templeList = [];
-
+const templesElement = document.querySelector("#temples");
 /* async displayTemples Function */
- async function  displayTemples(templeList) {
+function  displayTemples(templeList) {
     
-    for (let i = 0; i < templeList.lenght; i++)
-        {
-        let article = document.createElement("<article>");
-        
-        let h3 = document.createElement("h3");
-        h3.setAttribute("templeName", "createElement");
-        let h3Text = document.createTextNode(`${templeList[i].templeName}`)
-
-        h3.appendChild(h3Text);
-        let img = document.createElement("img");
-        
-        img.setAttribute("src", "createElement");
-        img.setAttribute("alt", "createElement");
-        img.src = templeList[i].imageUrl;
-        img.alt = templeList[i].location;
-        
-        article.appendChild(h3);
+    templeList.forEach(temple => {
+        const article = document.createElement("article");
+        const templeNameH3 = document.createElement("h3");
+        templeNameH3.innerHTML = temple.templeName;
+        const img = document.createElement("img");
+        img.src = temple.imageUrl;
+        img.alt = temple.location;
+        article.appendChild(templeNameH3);
         article.appendChild(img);
         templesElement.appendChild(article);
-        
-        
-        };
+    });
 };
 
 
 
 /* async getTemples Function using fetch()*/
 async function getTemples () {
-    displayTemples(templeList);
-    const requestUrl = "https://byui-cse.github.io/cse121b-ww-course/resources/temples.json"
-    const request = new Request(requestUrl);
-    const response = await fetch(request);
-    const temples = await response.json();
     
-    templeList.push(temples);
+    const response = await fetch( "https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    templeList = await response.json();
     displayTemples(templeList);
+    
     
     
     
@@ -50,22 +36,59 @@ async function getTemples () {
     
 }
 getTemples();
-console.log(templeList);
+
 
 
 
 
 
 /* reset Function */
+function reset(){
+    templesElement.innerHTML = null;
+    
 
+}
 
 /* sortBy Function */
 
 
+function sortBy(temples){
+   
+    
+    let filter = document.querySelector("#sortBy");
+    let filterValue = filter.value;
+   
+    reset();
+    switch(filterValue){
+        case "utah":
+            console.log(filterValue);
+           const utahTemples = temples.filter (function(temples){
+            return temples.location.includes("Utah")
+           }) ;
+           console.log(utahTemples);
+           displayTemples(utahTemples);
+           break
+        case "notutah":
+            console.log(filterValue);
+            const notUtahTemples = temples.filter (function(temples){
+                return (!temples.location.includes("Utah"))});
+            displayTemples(notUtahTemples);
+            break
+        case "older":
+            console.log(filterValue);
+            const olderTemples = temples.filter(temple => Date.parse(temple.dedicated) < new Date(1950, 0, 1));
+            displayTemples(olderTemples);
+            break
+        case "all":
+            console.log(filterValue);
+            displayTemples(temples);
+            break
 
+}}
 
 
 
 
 
 /* Event Listener */
+document.querySelector("#sortBy").addEventListener("change", () => {sortBy(templeList)});
